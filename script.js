b9e95c76b49e429aa7b87dc79e9a9db019764fe5
Pema@777 <pemanamgurtamang7@gmail.com>
2023-06-12 22:51:08 +0545

    git push -u

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function clicked(para){
+    if(para == 1){
+        document.getElementById('my-image').src='on-image.jpg';
+
+    }
+    else{
+        document.getElementById('my-image').src='off-image.jpg';
+    }
+}
